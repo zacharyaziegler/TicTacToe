2e125923d10c4f7380d0b13f1271a865d322c4b0
zacharyaziegler <zacharyaziegler@gmail.com>
2025-01-03 14:30:19 -0500

    Sign up functionality added using Supabase

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
+dotenv.config();
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
